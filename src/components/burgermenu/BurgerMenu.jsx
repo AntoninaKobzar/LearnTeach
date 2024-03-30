86a4865a0095b238a144c9107eb5d05fa214341d
Antonina Kobzar <antonina.kobzar@gmail.com>
2024-03-30 13:57:06 +0200

    1

--- src/components/burgermenu/BurgerMenu.jsx
+++ src/components/burgermenu/BurgerMenu.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTimes } from '@fortawesome/free-solid-svg-icons';
 import { Link } from 'react-router-dom';
--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import MainPage from '../mainPage/MainPage';
 import PupilPage from '../pupilPage/PupilPage'; 
 import TeacherPage from '../teacherPage/TeacherPage';
