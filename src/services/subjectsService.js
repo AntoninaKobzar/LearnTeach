0c8d58913f21ee3e021f3e396ac8bf0326f71e60
Antonina Kobzar <antonina.kobzar@gmail.com>
2024-04-04 09:08:20 +0300

    1

--- src/services/subjectsService.js
+++ src/services/subjectsService.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseUrl = 'http://localhost:3001/subjects'
+const baseUrl = 'http://localhost:3001/api/subjects'
 
 const getAll = () => {
   const request = axios.get(baseUrl);
